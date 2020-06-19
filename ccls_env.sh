b12b1eb86589b5df1481530a63150a90974ab1de
Ben <sh@quintessencelabs.com>
2020-06-19 13:07:13 +1000

    no ccls

--- ccls_env.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-git clone --depth=1 --recursive  https://github.com/MaskRay/ccls.git
-cd ccls
-cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCLANG_LINK_CLANG_DYLIB=on -DLLVM_LINK_LLVM_DYLIB=on
-cmake --build Release
-cd Release
-sudo make install
-
