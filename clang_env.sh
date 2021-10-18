ead4d83a739ed1779b4ddc099b9df05db99a558a
Benjamin-Shengming <shmhu@hotmail.com>
2021-10-18 15:38:34 +1100

    manjaro

--- clang_env.sh
+++ clang_env.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #install clangd c/cpp
-sudo apt install -y gcc
-sudo apt install -y g++
-sudo apt install -y clangd
-sudo apt install -y clang
+sudo pacman -Sy gcc
+sudo pacman -Sy g++
+sudo pacman -Sy clangd
+sudo pacman -Sy clang
--- python_env.sh
+++ python_env.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # install python
-sudo apt install -y python3-pip
+sudo pacman -Sy python-pip
 
 # install python
-sudo pip3 install -U setuptools
-sudo pip3 install 'python-language-server'
+sudo pip install -U setuptools
+sudo pip install 'python-language-server'
 
-sudo pip3 install neovim
+sudo pip install neovim
 sudo ln -s /usr/bin/python3 /usr/bin/python
 sudo ln -s /usr/bin/pip3 /usr/bin/pip
 
