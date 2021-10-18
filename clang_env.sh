3f3c470c2bec370d79c4cd10d8a3e3020678fa6f
Ben.Hu <sh@quintessencelabs.com>
2021-10-18 15:34:33 +1100

    update

--- clang_env.sh
+++ clang_env.sh
@@ -5,3 +5,15 @@ sudo apt install -y gcc
 sudo apt install -y g++
 sudo apt install -y clangd
 sudo apt install -y clang
+sudo apt install -y libclang-dev 
+
+echo "building ccls"
+git clone --depth=1 --recursive https://github.com/MaskRay/ccls
+cd ccls
+
+cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10 \
+    -DLLVM_INCLUDE_DIR=/usr/lib/llvm-10/include \
+    -DLLVM_BUILD_INCLUDE_DIR=/usr/include/llvm-10/
+
+cmake --build Release
